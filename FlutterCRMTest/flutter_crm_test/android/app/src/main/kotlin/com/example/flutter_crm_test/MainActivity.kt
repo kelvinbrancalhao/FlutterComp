543e4673d49f7c2b9f4c41d9a0a1189f787cdc57
Kelvin Ferri Brancalhão <kelvinbrancalhao@gmail.com>
2023-03-21 10:17:44 -0300

    Finalizado primeiro projeto
    Projeto de notas finalizado

---
+++ FlutterCRMTest/flutter_crm_test/android/app/src/main/kotlin/com/example/flutter_crm_test/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_crm_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ FlutterWidgetsRoadtrip/widget_roadtrip/android/app/src/main/kotlin/com/example/widget_roadtrip/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.widget_roadtrip
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
